fdb776320d7e1c7120679bc45253d5e3e9b7db14
Muhammad Shoaib <136969366+shoby056@users.noreply.github.com>
2025-01-01 13:59:26 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['fakestoreapi.com'], // Add external domain for images
+  },
+  // Other config options can go here
 };
 
 export default nextConfig;
